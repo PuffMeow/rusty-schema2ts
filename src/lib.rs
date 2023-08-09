c513096e5823ae06abcc2691915857855e53e3ef
JiquanWang99 <951873902@qq.com>
2023-08-09 22:28:16 +0800

    0.2.1

--- src/lib.rs
+++ src/lib.rs
@@ -17,10 +17,6 @@ use util::{
 #[macro_use]
 extern crate napi_derive;
 
-#[cfg(all(
-  not(all(target_os = "linux", target_env = "musl", target_arch = "aarch64")),
-  not(debug_assertions)
-))]
 #[global_allocator]
 static ALLOC: mimalloc_rust::GlobalMiMalloc = mimalloc_rust::GlobalMiMalloc;
 
